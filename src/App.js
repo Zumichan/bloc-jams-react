5efa3798381a1ef2568df66661b6245a5d5c7b2f
Azumi Callanan <azumi@Azumis-MacBook-Air.local>
2018-07-11 00:00:58 +0900

    Checkpoint Styling: In progress

--- src/App.js
+++ src/App.js
@@ -10,12 +10,25 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <nav className="navbar-link navbar-expand-md navbar-dark fixed-top bg-dark">
-            <Link to='/'>Bloc Jams Home</Link>
-            <Link to='/library'>Library</Link>
+          <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+            <Link className="navbar-brand" to='/'>Bloc Jams Home</Link>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                  <Link className="nav-link" to='/library'>Library</Link>
+                </li>
+              </ul>
+              <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+              </form>
+            </div>
           </nav>
         </header>
-          <main>
+          <main className='bg-dark'>
             <nav className="navbar-header">
               <Route exact path="/" component={Landing} />
               <Route path="/library" component={Library} />
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -4,31 +4,31 @@ const Landing = () => (
   <main role="main">
     <div className="jumbotron">
       <div className="container">
-        <h1 className="display-3">Turn the music up!</h1>
-          <p>Sample Sample Sample</p>
+        <h1 className="display-3 text-white">Turn the music up!</h1>
+          <p className="text-white">Sample Sample Sample</p>
           <p><a className="btn btn-primary btn-lg" href="#" role="button">Learn more >></a></p>
       </div>
     </div>
     <div class="container">
       <div className="row">
-        <div className="col-md-4">
+        <div className="col-md-4 text-white">
           <h2>Choose your music</h2>
           <p>The world is full of music; why should you have to music that someone else chose?</p>
           <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
         </div>
-        <div className="col-md-4">
+        <div className="col-md-4 text-white">
           <h2>Unlimited, streaming, ad-free</h2>
           <p>No arbitrary limits. No distractions.</p>
           <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
         </div>
-        <div className="col-md-4">
+        <div className="col-md-4 text-white">
           <h2>Mobile enabled</h2>
           <p>Listen to your music on the go. This streaming service is available on all mobile platforms.</p>
           <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
         </div>
       </div>
     </div>
-    <footer className="container">
+    <footer className="container text-white">
       <p>©︎Bloc Jams 2018</p>
     </footer>
   </main>
