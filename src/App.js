ca6d5028c6b7ea9b611fcc895edd8a917e8b15a4
Azumi Callanan <azumi@Azumis-MacBook-Air.local>
2018-07-18 22:41:17 +0900

    Checkpoint Styling: Completed

--- src/App.js
+++ src/App.js
@@ -12,19 +12,19 @@ class App extends Component {
         <header className="App-header">
           <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
             <Link className="navbar-brand" to='/'>Bloc Jams Home</Link>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="navbar-nav mr-auto">
-                <li class="nav-item">
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav mr-auto">
+                <li className="nav-item">
                   <Link className="nav-link" to='/library'>Library</Link>
                 </li>
               </ul>
-              <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-              </form>
+                <form className="form-inline my-2 my-lg-0">
+                  <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
+                  <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form>
             </div>
           </nav>
         </header>
